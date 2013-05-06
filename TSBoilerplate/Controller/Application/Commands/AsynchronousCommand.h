5a6aceff13127b5f6495a873afc4ef29c710f922
Tim Sawtell <timothy.sawtell@gmail.com>
2013-05-06 14:32:16 +1000

    logic update for asyn commands to not remove a command\'s parent\'s subcommand from the array, but rather check if any of them are still isFinished:no. Fixes threading issue

--- TSBoilerplate/Controller/Application/Commands/AsynchronousCommand.h
+++ TSBoilerplate/Controller/Application/Commands/AsynchronousCommand.h
@@ -1,15 +1,15 @@
 /*
  Copyright (c) 2012 Tim Sawtell
  
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
- to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  IN THE SOFTWARE.
  */
 
@@ -17,17 +17,45 @@
 
 typedef void(^commandCompletionBlock)(NSError *error);
 
+/*
+ * A command (which is an NSOperation) which will be put onto an NSOperationQueue by TSCommandRunner. The queue will either be a differnet thread to main or the main thread based on multiThreaded
+ */
+
 @interface AsynchronousCommand : Command
 
+/** The block of code to run after this command has finished */
 @property (nonatomic, copy) commandCompletionBlock commandCompletionBlock;
-@property (nonatomic, assign, getter = isMultiThreaded) BOOL multiThreaded;
-// these getters are used to tell NSOperationQueue that the task has actually finished
-@property (nonatomic, assign, getter = isFinished) BOOL finished;
-@property (nonatomic, assign, getter = isExecuting) BOOL executing;
-@property (nonatomic, strong) AsynchronousCommand *parentCommand;
-@property (nonatomic, strong) NSMutableArray *subCommands;
 
+/** if YES then SPCommandRunner will put this command onto a shared NSOperationQueue (not main). If NO then it will go onto [NSOperationQueue mainQueue] */
+@property (nonatomic, assign, getter = isMultiThreaded) BOOL multiThreaded;
+
+/** A reference to the parent command that owns this command. */
+@property (nonatomic, strong) AsynchronousCommand *parentCommand;
+
+/** these getters are used to tell NSOperationQueue that the task has actually finished */
+@property (nonatomic, assign, getter = isFinished) BOOL finished;
+
+/** these getters are used to tell NSOperationQueue that the task is executing */
+@property (nonatomic, assign, getter = isExecuting) BOOL executing;
+
+/** Add a command as a subcommand
+ * @param Command command - the command to add as a subcommand
+ */
+- (void)addSubCommand:(Command *)command;
+
+/** Remove a subcommand
+ * @param Command command - the command to remove as a subcommand
+ */
+- (void)removeSubCommand:(Command *)command;
+
+/** the array of subCommands for this command*/
+- (NSArray *)subCommands;
+
+/** Tell the command to cancel (won't guarantee the command will stop dead in its tracks) */
 - (void)cancel;
+
+/** Finish processing and let the NSOperationQueue remove this command */
 - (void)finish;
+
 
 @end
--- TSBoilerplate/Controller/Application/Commands/AsynchronousCommand.m
+++ TSBoilerplate/Controller/Application/Commands/AsynchronousCommand.m
@@ -1,27 +1,31 @@
 /*
  Copyright (c) 2012 Tim Sawtell
  
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
- to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  IN THE SOFTWARE.
  */
 
 #import "AsynchronousCommand.h"
 
+@interface AsynchronousCommand()
+@property (nonatomic, strong) NSMutableArray *_subCommands;
+@end
+
 @implementation AsynchronousCommand
-@synthesize commandCompletionBlock, multiThreaded, finished, executing, subCommands;
+@synthesize _subCommands, commandCompletionBlock, multiThreaded, finished, executing, parentCommand;
 
 - (id)init
 {
-    if (self = [super init]) {
-        self.subCommands = [NSMutableArray array];
+    if (self = [super init]){
+        _subCommands = [NSMutableArray array];
     }
     return self;
 }
@@ -38,19 +42,18 @@
     __weak AsynchronousCommand *weakSelf = self;
     self.completionBlock = ^{
         __strong AsynchronousCommand *strongSelf = weakSelf;
-        if (strongSelf.commandCompletionBlock != NULL && !strongSelf.isCancelled) {
+        if (NULL != strongSelf.commandCompletionBlock && !strongSelf.isCancelled) {
             dispatch_sync(dispatch_get_main_queue(), ^{
                 strongSelf.commandCompletionBlock(strongSelf.error);
             });
         }
-        strongSelf.commandCompletionBlock = nil;
+        strongSelf.completionBlock = nil; // needed to do this otherwise retain cycle
     };
     //subclass should override execute to perform the logic of the command
     [self execute];
     
     //subclass MUST call [self finish] at the appropriate time, generally in the commandCompletionBlock
 }
-
 
 - (BOOL)isMultiThreaded
 {
@@ -59,7 +62,7 @@
 
 - (void)cancel
 {
-    for (Command *cmd in self.subCommands) {
+    for (Command *cmd in [self subCommands]) {
         [cmd cancel];
     }
     //in subclasses add your logic to cancel the processing in execute.
@@ -83,15 +86,44 @@
 
 - (void) finish
 {
+    // if I am a subcommand (I am if I have a parentCommand) then check if my parent has no other sub commands running. If so, then finish the parent as well
     if (nil != self.parentCommand) {
-        [self.parentCommand.subCommands removeObject:self];
-        if ([self.parentCommand.subCommands count] == 0) {
-            [self setFinished: YES];
+        BOOL runningSubCommands = NO;
+        for (AsynchronousCommand *cmd in self.parentCommand.subCommands) {
+            if ([cmd isEqual:self]) continue; // don't look at self in this loop
+            if (!cmd.isFinished) {
+                runningSubCommands = YES;
+                break;
+            }
+        }
+        if (!runningSubCommands) {
+            [self setFinished: YES]; // have to finish self before the parent, makes sence
             [self.parentCommand finish];
             return;
         }
     }
     [self setFinished: YES];
+}
+
+- (void)addSubCommand:(Command *)command
+{
+    [_subCommands addObject:command];
+    if ([command isKindOfClass:[AsynchronousCommand class]]) {
+        __weak AsynchronousCommand *async = (AsynchronousCommand *)command;
+        async.parentCommand = self;
+    }
+}
+
+- (void)removeSubCommand:(Command *)command
+{
+    if ([_subCommands containsObject:command]) {
+        [_subCommands removeObject:command];
+    }
+}
+
+- (NSArray *)subCommands
+{
+    return _subCommands;
 }
 
 @end
