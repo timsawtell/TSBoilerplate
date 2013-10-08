070c81992ccd8b112d56da3c80434711ca6efd3d
Tim Sawtell <timsa@gji.com.au>
2013-10-08 13:13:04 +1000

    added weakSelf if nil check

--- App/Controller/ViewControllers/TweetsViewController.m
+++ App/Controller/ViewControllers/TweetsViewController.m
@@ -62,6 +62,7 @@
     twitterCommand.includeEntities = NO;
     twitterCommand.tweetCount = kNumTweets;
     twitterCommand.commandCompletionBlock = ^ (NSError *error) {
+        if (nil == weakSelf) return;
         __strong typeof(self) strongSelf = weakSelf;
         [strongSelf hideActivityScreen];
         if (error != nil) {
